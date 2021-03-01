7a1fec96c7c0a746d76bcb0cd5f1b6ed7bc61aff
isriom <62965230+isriom@users.noreply.github.com>
2021-03-01 13:16:45 -0600

    Complete List Commands: Add_### add a new Node to the list with (#) as a number del_### del a Node with number # from the Node stp stop and exit of the app

---
+++ Collector.cpp
@@ -0,0 +1,6 @@
+//
+// Created by 2001i on 28/2/2021.
+//
+
+#include "Collector.h"
+
---
+++ Node.h
@@ -0,0 +1,13 @@
+//
+// Created by 2001i on 28/2/2021.
+//
+
+#ifndef COLLECTOR_NODE_H
+#define COLLECTOR_NODE_H
+
+#include <cstdlib>
+
+
+
+
+#endif //COLLECTOR_NODE_H
